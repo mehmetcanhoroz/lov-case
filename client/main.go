95eae69c302fb79cd26a06309c0ac6ec1f99654e
Mehmet Canhoroz <mehmetcanhoroz@gmail.com>
2022-10-17 00:22:55 +0300

    Updated build

--- client/main.go
+++ client/main.go
@@ -18,6 +18,7 @@ var (
 )
 
 func main() {
+	fmt.Printf("%v \n", os.Args[1])
 	if err := run(os.Args, os.Stdout); err != nil {
 		_, err := fmt.Fprintf(os.Stderr, "%s\n", err)
 		if err != nil {
@@ -32,6 +33,11 @@ func run(args []string, stdout io.Writer) error {
 	flags.StringVar(&calculation, "method", "all", "-method add")
 	flags.IntVar(&n1, "a", 1, "-a 0")
 	flags.IntVar(&n2, "b", 1, "-b 0")
+
+	fmt.Println(flags)
+	fmt.Println(calculation)
+	fmt.Println(n1)
+	fmt.Println(n2)
 
 	err := godotenv.Load()
 	if err != nil {
