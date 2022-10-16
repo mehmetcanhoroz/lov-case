cd6941a682c92a513bc50f77143d0c4836ac0faa
Mehmet Canhoroz <mehmetcanhoroz@gmail.com>
2022-10-16 23:57:25 +0300

    changed func name

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,9 +11,9 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-build_function() {
+buildapp() {
   echo -e "${GREEN}Building $1...${NC}"
   env GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o ./bin/"$where" ./"$from"
 }
 
-build_function "$1" "$2" "$3"
+buildapp "$1" "$2" "$3"
